aef717ed3ce765f0331052db09e7bece05ccbe12
Morgan Nelson <mnelson@steele-forge.com>
2016-06-15 13:43:41 -0500

    add name attribute to metadata.rb
    or spiceweasel loses mind

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
- maintainer       "AJ Christensen"
+maintainer       "AJ Christensen"
 maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.7.1"
+name             "jenkins"
 
 %w(apache2 nginx runit java chef_gem yumrepo).each { |cb| depends cb }
 %w(iptables yum apt).each { |cb| recommends cb }
